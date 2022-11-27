d00b191a5bb4df2e6b83745964138348cacb4280
gromero <gromero@createitreal.com>
2022-11-27 20:15:41 +0100

    Add subprocess for training

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,3 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def application():
     return render_template("index.html")
+
+
+@app.route("/train", methods=["GET", "POST"])
+def data():
+    def inner():
+        proc = subprocess.Popen(
+            [
+                "python",
+                "-u",
+                "train.py",
+            ],
+            shell=True,
+            stdout=subprocess.PIPE,
+        )
+
+        for line in iter(proc.stdout.readline, ""):
+            yield line.rstrip().decode("utf-8") + "<br/>\n"
+
+    return Response(
+        inner(), mimetype="text/html"
+    )  # text/html is required for most browsers to show
