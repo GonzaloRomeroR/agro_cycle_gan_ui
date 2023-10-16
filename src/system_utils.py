bc164da757b6b4cc8e81b7220b1985c2981dc089
gromero <gromero@createitreal.com>
2023-10-16 18:59:25 +0200

    More cleanup

--- src/system_utils.py
+++ src/system_utils.py
@@ -1,4 +1,6 @@
 import subprocess
+from psutil import Process
+from flask import current_app
 
 
 def run_subprocess(process_list, socket, output):
@@ -6,6 +8,14 @@ def run_subprocess(process_list, socket, output):
     Run subprocess and write the output to container
     """
     with subprocess.Popen(process_list, stdout=subprocess.PIPE, text=True) as process:
+        current_app.config[output] = process.pid
         for line in process.stdout:
             socket.emit(output, {"data": line})
         process.wait()
+
+
+def kill_process(pid):
+    parent = Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    parent.kill()
